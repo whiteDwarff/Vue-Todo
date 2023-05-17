494269c37e0da1b8f36a21f34caca8ff6042263a
whiteDwarf <munstarrrrr@gmail.com>
2023-05-17 23:34:48 +0900

    0517 first commit

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+export default {
+  data: function() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
+  }
+}
+</script>
+
+<style>
+
+</style>
