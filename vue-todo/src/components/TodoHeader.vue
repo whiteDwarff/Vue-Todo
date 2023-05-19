4fffed9f6596da72fca34faf62b6d8981d21ed74
whiteDwarf <munstarrrrr@gmail.com>
2023-05-19 09:59:16 +0900

    header-input component commit

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,16 @@
 <template>
-    <div>
-        header
-    </div>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
 </template>
-
 <script>
-export default {
-
-}
+    // scoped ? style을 해당 컴포넌트에만 적용하겠다 ~ 
+    // 상속받지않음.
 </script>
-
-<style>
-
+<style scoped>
+h1 {
+    color: #2f3852;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
 </style>
--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    input
+    <input type="text" v-model="newTodoItem">
+    <button @click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
+  data : function() {
+    return { 
+      newTodoItem: "",
+    }
+  },
+  methods: {
+    addTodo : function() {
+      console.log(this.newTodoItem);
 
+      localStorage.setItem("ad", this.newTodoItem);
+      // 저장하는 로직
+      this.newTodoItem = "";
+    },
+
+  }
 }
 </script>
 
