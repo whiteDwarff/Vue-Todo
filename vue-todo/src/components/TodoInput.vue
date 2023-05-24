e7e8ff2919531c202d659278559a771c4db8e10f
whiteDwarf <munstarrrrr@gmail.com>
2023-05-24 12:50:08 +0900

    0524 input & list update

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem">
-    <button @click="addTodo">add</button>
+  <div class="inputBox shadow">
+    <!-- v-model : to-way binding -->
+    <!-- v-on:keyup.enter="", enter를 사용해도 event 실행할거야 ~ -->
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <span class="addContainer">
+      <i class="fa-solid fa-plus addBtn" @click="addTodo"></i>
+    </span>
   </div>
 </template>
 
@@ -12,19 +16,53 @@ export default {
       newTodoItem: "",
     }
   },
-  methods: {
+  methods: {    
     addTodo : function() {
-      console.log(this.newTodoItem);
-
-      localStorage.setItem("ad", this.newTodoItem);
-      // 저장하는 로직
-      this.newTodoItem = "";
+      if(this.newTodoItem != '') {
+        // 동일한 text 값으로 key, value 저장
+        const obj = {
+          completed: false,
+          item: this.newTodoItem
+        };
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+        this.clearInput();
+      }
     },
+    clearInput : function() {
+      this.newTodoItem = "";
+    }
 
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: #fff;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+  display: flex;
+  width: 50%;
+  justify-content: space-between;
+  margin: 0 auto;
+}
+.inputBox input{
+  border-style: none;
+  font-size: 0.9rem;
+  width: 100%;
+}
+.addContainer {
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: #fff;
+  vertical-align: middle;
+}
 </style>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,15 +1,88 @@
 <template>
   <div>
-    list
+    <ul>
+      <!-- v-for 실행 시 순서를 부여해주는 index가 있다 (default) -->
+      <li v-for="(todoItem, index) in todoItems" :key="todoItem.item" class="shadow">
+        <i @click="toggleComplete(todoItem)" class="fa-solid fa-check checkBtn" :class="{checkBtnCompleted: todoItem.completed}"></i>
+        <!-- v-bind:를 class에 동적인 값을 부여한다.  -->
+        <span :class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
+        <!-- li에서 실행된 todoItem과 index를 받아온다. -->
+        <span @click="removeTodo(todoItem, index)" class="removeBtn">
+          <i class="fa-solid fa-trash-can"></i>
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      todoItems: [],
+    }
+  },
+  // life cycle
+  // 인스턴스가 생성되는 시점에 실행되는 함수
+  created: function() {
+    if(localStorage.length) {
+      for(let i = 0; i < localStorage.length; i++) {
+        // localStorage의 key의 개수만큼 반복해서 todoItems에 담기
+        // data를 object로 변환해서 localStorage에서 가져온 i를 todoItems에 담아 ~~
+        this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+      }
+    }
+  },
+  methods: {
+    removeTodo: function(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      // data에 저장된 todoItems의 배열에서 v-for실행시 생성된 index를 삭제함
+      this.todoItems.splice(index);
+    },
+    toggleComplete: function(todoItem) {
+      // NOT 연산자를 사용하여 true이면 false로, false면 true로 변경시킴 !!!!! 좋은 로직이네 ~~
+      todoItem.completed = !todoItem.completed;
+      // localStorage의 데이터 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  ul { 
+    list-style: none;
+    padding-left: 0;
+    margin-top: 0;
+    text-align: left;
+    }
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background-color: #fff;
+    border-radius: 5px;
+  }
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+    cursor: pointer;
+  }
+  .checkBtnCompleted {
+    color: #b3adad;
+  }
+  .textCompleted {
+    text-decoration: line-through;
+    color: #b3adad;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+    cursor: pointer;
+  }
 </style>
